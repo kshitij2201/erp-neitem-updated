88be415d0a8335108922e865444df143a2107d84
Nayan0406git config --global user.email <nayannikhare83@gmail.com>
2025-11-17 12:34:31 +0530

    add cors policy

--- backend-smp/server.js
+++ backend-smp/server.js
@@ -204,6 +204,7 @@ const corsOptions = {
       "http://127.0.0.1:4000",
       "http://127.0.0.1:5173",
       "http://127.0.0.1:5174",
+      "https://erpdemo.tarstech.in",
     ];
 
     // Add production URLs if specified
